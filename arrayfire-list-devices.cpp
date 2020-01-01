25d2d562bc17cbcfef46a50865fe949dad399900
Nicholas Corgan <n.corgan@gmail.com>
2019-12-31 19:15:46 -0600

    Implementation

--- arrayfire-list-devices.cpp
+++ arrayfire-list-devices.cpp
@@ -1,7 +1,28 @@
 // Copyright (c) 2019 Nicholas Corgan
 // SPDX-License-Identifier: BSD-3-Clause
 
+#include <arrayfire.h>
+
+#include <iostream>
+#include <vector>
+
 int main(int, char**)
 {
-    return 0;
+    std::cout << "ArrayFire Config Info" << std::endl << std::endl;
+
+    const int availableBackends = af::getAvailableBackends();
+    const std::vector<af::Backend> backends =
+    {
+        AF_BACKEND_CUDA,
+        AF_BACKEND_OPENCL,
+        AF_BACKEND_CPU
+    };
+    for(af::Backend backend: backends)
+    {
+        if(availableBackends & backend)
+        {
+            af::setBackend(backend);
+            af::info();
+        }
+    }
 }
